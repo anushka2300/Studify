4cddc9508a8d9d085412f1e3ce3d3fccc3192039
anushka2300 <122161237+anushka2300@users.noreply.github.com>
2025-02-15 14:16:31 +0530

    Update App.jsx

--- notes-collab/src/App.jsx
+++ notes-collab/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React from "react";
 import Branch from './components/Branche/Branche'
 import Subject from './components/subjects/subject';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
