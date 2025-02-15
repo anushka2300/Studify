afa0716eb17f8c7278e68a2e4518e7d87bce6ff1
anushka2300 <122161237+anushka2300@users.noreply.github.com>
2025-02-15 12:17:48 +0530

    branch

--- notes-collab/src/App.jsx
+++ notes-collab/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Branch from './components/branches/branch'
+import Branch from './components/Branche/Branche'
 import Subject from './components/subjects/subject';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/login/login';
---
+++ notes-collab/src/components/Branche/Branche.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Card from "../branchCard/card";
+import './Branche.css'
+import Navbar from "../navbar/navbar";
+
+const branches = [
+  { title: "Computer Science and Engineering", icon: "🔍", description: "Explore CS." },
+  { title: "Information Technology", icon: "💻", description: "Explore IT." },
+  { title: "Electrical and Electronics Engineering", icon: "🪫", description: "Explore EEE." },
+  { title: "Electronics and Communication Engineering", icon: "⚙️", description: "Explore ECE." },
+  { title: "Mechanical Engineering", icon: "🦾", description: "Explore Mechanical." },
+  { title: "Civil Engineering", icon: "🌉", description: "Explore Civil." },
+  { title: "Chemical Engineering", icon: "🧪", description: "Explore Chemical." },
+  { title: "Aerospace Engineering", icon: "✈️", description: "Explore Aerospace." },
+  { title: "Automobile Engineering", icon: "🚗", description: "Explore Automobile." },
+  { title: "Metallurgical and Materials Engineering", icon: "🧇", description: "Explore Metallurgical and Materials." },
+
+];
+
+
+const Branch = () => {
+  return (
+    <>
+    <Navbar/>
+    <div className="app">
+      <header className="header">
+        <h1>BTech Branches</h1>
+      </header>
+      <div className="branch-container">
+        {branches.map((branch, index) => (
+          <Card
+            key={index}
+            title={branch.title}
+            icon={branch.icon}
+            description={branch.description}
+          />
+        ))}
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Branch;
